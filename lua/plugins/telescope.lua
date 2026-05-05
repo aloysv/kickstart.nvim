b1edc3206de72db84fbadcd8ccf7735c084e9ee9
aloysv <aloys.vreeswijk@gmail.com>
2026-05-05 10:20:14 +0200

    remove cyberdream-light

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -79,6 +79,11 @@ return {
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+      local function colorscheme_picker()
+        local colors = vim.tbl_filter(function(color) return color ~= 'cyberdream-light' end, vim.fn.getcompletion('', 'color'))
+        builtin.colorscheme { enable_preview = true, colors = colors }
+      end
+
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
@@ -87,7 +92,7 @@ return {
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>st', function() builtin.colorscheme { enable_preview = true } end, { desc = '[S]earch [T]hemes' })
+      vim.keymap.set('n', '<leader>st', colorscheme_picker, { desc = '[S]earch [T]hemes' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
